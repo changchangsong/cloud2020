15cc82bf22aacad48323ae423ad5c9cd2cb2c3a8
songchang <1334452063@qq.com>
2020-04-22 22:10:48 +0800

    消费者

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,34 @@
+package com.atguigu.springcloud.entities;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 17:22
+ */
+public class Payment {
+    private Long id;
+    private String serial;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, String serial) {
+        this.id = id;
+        this.serial = serial;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
+}
