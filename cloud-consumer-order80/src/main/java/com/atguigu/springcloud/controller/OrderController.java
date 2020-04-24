6e955f244afe95265253c81a3a26619f19d629fa
songchang <1334452063@qq.com>
2020-04-24 22:58:13 +0800

    erake集群

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -19,13 +16,17 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController
 {
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    // 单机版
+    //public static final String PAYMENT_URL = "http://localhost:8001";
+
+    // 集群版
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
     private RestTemplate restTemplate;
 
     @PostMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment)
+    public CommonResult<Payment> create(@RequestBody Payment payment)
     {
         return restTemplate.postForObject(PAYMENT_URL +"/payment/create",payment,CommonResult.class);
     }
