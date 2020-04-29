dac5eb11043e00f4cfc94a0b91893c25504c0535
songchang <1334452063@qq.com>
2020-04-29 22:21:54 +0800

    hystrix服务降级 包括服务端服务降级  客户端服务降级  已经客户端全局服务降级（运行时异常 运行时超时 挡机）

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.atguigu.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -21,20 +23,20 @@ public class PaymentService
         return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
     }
 
-//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
-//            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
-//    })
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
+    })
     public String paymentInfo_TimeOut(Integer id)
     {
-        //int age = 10/0;
+//        int age = 10/0;//运行时报错
         try { TimeUnit.MILLISECONDS.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): ";
     }
+    public String paymentInfo_TimeOutHandler(Integer id)
+    {
+        return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
+    }
 
-//    public String paymentInfo_TimeOutHandler(Integer id)
-//    {
-//        return "线程池:  "+Thread.currentThread().getName()+"  8001系统繁忙或者运行报错，请稍后再试,id:  "+id+"\t"+"o(╥﹏╥)o";
-//    }
 //
 //    //=====服务熔断
 //    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
