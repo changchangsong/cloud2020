71e5430a1d2d9767cde0fe62caf1da1e8e260319
sc <1334452063@qq.com>
2020-05-12 14:28:23 +0800

    sentinel 流控规则（QPS 、线程数） 热点规则（参数例外项）

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,9 +1,10 @@
 package com.atguigu.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @auther zzyy
@@ -31,33 +32,33 @@ public class FlowLimitController
     @GetMapping("/testD")
     public String testD()
     {
-        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
-        System.out.println("testD 测试RT");
+//        try { TimeUnit.SECONDS.sleep(1); } catch (InterruptedException e) { e.printStackTrace(); }
+//        System.out.println("testD 测试RT");
 
-//        System.out.println("testD 异常比例");
-//        int age = 10/0;
+        System.out.println("testD 异常比例");
+        int age = 10/0;
         return "------testD";
     }
-//
-//    @GetMapping("/testE")
-//    public String testE()
-//    {
-//        System.out.println("testE 测试异常数");
-//        int age = 10/0;
-//        return "------testE 测试异常数";
-//    }
-//
-//    @GetMapping("/testHotKey")
-//    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
-//    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
-//                             @RequestParam(value = "p2",required = false) String p2)
-//    {
-//        //int age = 10/0;
-//        return "------testHotKey";
-//    }
-//    public String deal_testHotKey (String p1, String p2, BlockException exception)
-//    {
-//        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
-//    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        System.out.println("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2)
+    {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+    public String deal_testHotKey (String p1, String p2, BlockException exception)
+    {
+        return "------deal_testHotKey,o(╥﹏╥)o";  //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
+    }
 
 }
